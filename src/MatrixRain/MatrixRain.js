23e2aa2f8e2c8879ac32982519472312937124d1
n00b0dyy <you@example.com>
2025-02-03 10:43:05 +0100

    Responsivity

--- src/MatrixRain/MatrixRain.js
+++ src/MatrixRain/MatrixRain.js
@@ -54,12 +54,11 @@ const MatrixRain = () => {
     <canvas
       ref={canvasRef}
       style={{
-        position: "absolute",
+        position: "relative",
         top: 0,
         left: 0,
         width: "100%",
         height: "100%",
-        zIndex: 2,
       }}
     />
   );
