0614958d04bfda282959dfde1d01ec75d952062f
n00b0dyy <you@example.com>
2025-02-03 13:01:21 +0100

    Hover menus issues

--- src/InteractiveMap/InteractiveMap.js
+++ src/InteractiveMap/InteractiveMap.js
@@ -132,9 +132,10 @@ const InteractiveMap = () => {
       dragging={true}
     >
       <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+        attribution='Tiles &copy; <a href="https://www.esri.com">Esri</a> &mdash; Esri, DeLorme, NAVTEQ'
       />
+
       {locations.map(location => (
         <Marker
           key={location.id}
