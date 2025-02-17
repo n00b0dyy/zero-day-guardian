a503887677f2cee77f5323e776d70cd45bcda974
n00b0dyy <you@example.com>
2025-02-17 09:41:34 +0100

    articles navigation logic, better graphic experience

--- src/Articles/ArticlePage.js
+++ src/Articles/ArticlePage.js
@@ -4,22 +4,27 @@ import { useTranslation } from "react-i18next";
 import "./ArticlePage.css";
 
 const ArticlePage = () => {
-  const { id } = useParams(); // Pobieranie ID artykułu z URL
+  const { id } = useParams();
   const [article, setArticle] = useState(null);
-  const [error, setError] = useState(null); // Obsługa błędów
+  const [error, setError] = useState(null);
   const { i18n, t } = useTranslation();
+
+  useEffect(() => {
+    const hasRefreshed = sessionStorage.getItem("hasRefreshed");
+    if (!hasRefreshed) {
+      sessionStorage.setItem("hasRefreshed", "true");
+      window.location.reload();
+    }
+  }, []);
 
   useEffect(() => {
     const fetchArticle = async () => {
       try {
-        // Pobieranie artykułów w zależności od języka
         const response = await fetch(`/data/articles.${i18n.language}.json`);
         if (!response.ok) {
           throw new Error(`Error fetching articles: ${response.statusText}`);
         }
         const data = await response.json();
-
-        // Szukanie konkretnego artykułu na podstawie ID
         const foundArticle = data.find(item => item.id === parseInt(id));
         if (!foundArticle) {
           throw new Error("Article not found");
@@ -27,14 +32,13 @@ const ArticlePage = () => {
         setArticle(foundArticle);
       } catch (err) {
         console.error("Error fetching article:", err);
-        setError(err.message); // Ustawienie błędu
+        setError(err.message);
       }
     };
 
     fetchArticle();
   }, [id, i18n.language]);
 
-  // Wyświetlenie błędu (jeśli wystąpił)
   if (error) {
     return (
       <p className="error-message">
@@ -43,12 +47,10 @@ const ArticlePage = () => {
     );
   }
 
-  // Wyświetlenie komunikatu ładowania
   if (!article) {
     return <p className="loading-message">{t("article.loading")}</p>;
   }
 
-  // Renderowanie artykułu
   return (
     <div className="article-page">
       <h1 className="article-title">{article.title}</h1>
@@ -56,7 +58,7 @@ const ArticlePage = () => {
         <img
           src={article.image}
           alt={article.title}
-          className="article-image"
+          className="article-main-image"
           loading="lazy"
         />
       )}
