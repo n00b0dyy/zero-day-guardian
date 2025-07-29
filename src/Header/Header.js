b0ee08c90b2c66447fe89e303b2b340944875639
Kamil Lukas <162102955+n00b0dyy@users.noreply.github.com>
2025-07-29 13:17:32 +0200

    Update Header.js

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,11 +2,9 @@ import React, { memo, useCallback, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./Header.css";
-import Logo from "../Logo/Logo";
 import GlitchText from "../GlitchText/GlitchText";
 import Menu from "../Menu/Menu1";
 import Menu2 from "../Menu/Menu2";
-import MatrixRain from "../MatrixRain/MatrixRain.js";
 
 const useDebouncedCallback = (callback, delay) => {
   const timerRef = useRef(null);
@@ -25,7 +23,7 @@ const useDebouncedCallback = (callback, delay) => {
 };
 
 const Header = () => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const changeLanguage = useDebouncedCallback(lng => {
     i18n.changeLanguage(lng);
@@ -33,41 +31,36 @@ const Header = () => {
 
   return (
     <header className="header">
-      {/* MatrixRain pozostaje na samym dole */}
-      <MatrixRain />
-      {/* Logo ma wyższy z-index i jest nad MatrixRain */}
       <div className="header-logo">
-        <Link to="/about-me">
-          <Logo />
+        <Link to="/about-me" className="about-link">
+          {t("aboutMeLink")}
         </Link>
       </div>
-      <div className="header-content">
-        <div className="header-glitch">
-          <Link to="/" className="glitch-link">
-            <GlitchText />
-          </Link>
-        </div>
-        <div className="header-menus">
-          <Menu />
-          <Menu2 />
-        </div>
-        <div className="language-switcher">
-          <button onClick={() => changeLanguage("en")} className="lang-btn">
-            EN
-          </button>
-          <button onClick={() => changeLanguage("pl")} className="lang-btn">
-            PL
-          </button>
-          <button onClick={() => changeLanguage("fr")} className="lang-btn">
-            FR
-          </button>
-          <button onClick={() => changeLanguage("no")} className="lang-btn">
-            NO
-          </button>
-          <button onClick={() => changeLanguage("ru")} className="lang-btn">
-            RU
-          </button>
-        </div>
+      <div className="header-glitch">
+        <Link to="/" className="glitch-link">
+          <GlitchText />
+        </Link>
+      </div>
+      <div className="header-menus">
+        <Menu />
+        <Menu2 />
+      </div>
+      <div className="language-switcher">
+        <button onClick={() => changeLanguage("en")} className="lang-btn">
+          EN
+        </button>
+        <button onClick={() => changeLanguage("pl")} className="lang-btn">
+          PL
+        </button>
+        <button onClick={() => changeLanguage("fr")} className="lang-btn">
+          FR
+        </button>
+        <button onClick={() => changeLanguage("no")} className="lang-btn">
+          NO
+        </button>
+        <button onClick={() => changeLanguage("ru")} className="lang-btn">
+          RU
+        </button>
       </div>
     </header>
   );
