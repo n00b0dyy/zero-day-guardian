137abeb5577e97b8d1b9a8468f3a3e2442421323
Kamil Lukas <162102955+n00b0dyy@users.noreply.github.com>
2025-07-29 14:39:32 +0200

    Update Logo.js

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -1,12 +1,1 @@
-import React from "react";
-import "./Logo.css";
 
-const Logo = () => {
-  return (
-    <div className="logo-wrapper">
-      <img src="/images/logo/logo.png" alt="Logo" className="logo" />
-    </div>
-  );
-};
-
-export default Logo;
